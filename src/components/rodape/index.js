884bfbb2e68326850174dca10ddd0533be5c99d8
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-02-25 20:28:30 -0300

    log caprem

--- src/components/rodape/index.js
+++ src/components/rodape/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import barbo from "../../assets/logo/logo-barbo.png";
-import caprem from "../../assets/logo/logo-caprem.png";
+import caprem from "../../assets/logo/logo-branco.png";
 import facebook from "../../assets/icon/face.png";
 import instagram from "../../assets/icon/insta.png";
 
@@ -18,19 +18,19 @@ export default function Page(props) {
   return (
       <div className="bg-red w-full">
 
-        <div className="flex flex-wrap justify-center p-4 pb-0">
+        <div className="flex flex-col md:flex-row justify-center ml-4 md:ml-52 pl-4 md:pl-52">
 
-          <div className="flex items-center mx-6 md:mx-0 my-2 md:my-8">
-            <div className="pr-4 md:pr-24">
+          <div className="flex items-center my-2">
+            <div className="w-1/2 md:w-96 pl-4 md:pl-6 mr-10 md:mr-16">
               <img src={barbo} alt="Barbo Epreendimentos" />
             </div>
 
-            <div className="pr-2 md:pr-24">
+            <div className="w-2/3 md:w-96 pr-8 md:pr-16">
               <img src={caprem} alt="Caprem Construtora" />
             </div>
           </div>
 
-          <div className="items-center  pt-10 md:pt-16">
+          <div className="m-8 mr-10 md:mr-52 pr-10 md:pr-64 pt-0 md:pt-20">
             <div className="flex flex-row ">
               <a className="w-8 md:w-10 pl-2 md:pl-0 "
                 href="https://www.facebook.com/capremconstrutora/"
@@ -45,10 +45,11 @@ export default function Page(props) {
                 <img src={instagram} alt="instagram" />
               </a>
 
-            <div className="flex font-small md:font-medium family-montmedium items-center pl-2 md:pl-0">@capremconstrutora</div>
+            <div className="flex font-small md:font-medium family-montmedium items-center pl-2 md:pl-0">
+              @capremconstrutora</div>
             </div>
 
-            <div className="font-medium md:font-regular family-medium tracking-widest ml-2 md:ml-0 mb-8 md:mb-14">
+            <div className="font-medium md:font-regular family-medium tracking-widest ml-2 md:ml-0 mb-0 md:mb-14">
               <a href="https://www.caprem.com.br/"
                 target="_blank"
                 rel="noreferrer">www.caprem.com.br
@@ -57,7 +58,7 @@ export default function Page(props) {
           </div>
 
         </div>
-        <div className="family-light sm:font-xs md:font-medium lg:regular mt-4 md:mt-10 pb-6 text-center">
+        <div className="family-light font-small md:font-medium mt-4 md:mt-10 pb-6 text-center">
           Caprem Construtora©2022. Todos os direitos reservados
         </div>
       </div>
