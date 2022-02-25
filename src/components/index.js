88f9028d0a5c122e9530e66d39dfca7aab182a0f
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-02-25 20:34:05 -0300

    logo caprem

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,24 @@
-// import SaibaMais from "./saiba-mais";
+import SaibaMais from "./saiba-mais";
 
-import Topo from "./topo";
-import Familia from "./familia";
-import Entreter from "./entreter";
-import Descrição from "./descricao";
-import Fotos from "./fotos";
-import Plantas from "./plantas";
-import Mapa from "./mapa";
+// import Topo from "./topo";
+// import Familia from "./familia";
+// import Entreter from "./entreter";
+// import Descrição from "./descricao";
+// import Fotos from "./fotos";
+// import Plantas from "./plantas";
+// import Mapa from "./mapa";
 import Rodape from "./rodape";
 
 
 export {
-// SaibaMais,
+SaibaMais,
 
-  Topo,
-  Familia,
-  Entreter,
-  Descrição,
-  Fotos,
-  Plantas,
-  Mapa,
+  // Topo,
+  // Familia,
+  // Entreter,
+  // Descrição,
+  // Fotos,
+  // Plantas,
+  // Mapa,
   Rodape,
 };
